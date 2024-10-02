0a5f31a83358d36717b6710cf5ead9e49f759696
zhehuaichen <dian.chenzhehuai@gmail.com>
2024-10-01 17:58:12 -0700

    lora distributed
    Signed-off-by: zhehuaichen <dian.chenzhehuai@gmail.com>

--- nemo/collections/nlp/parts/mixins/nlp_adapter_mixins.py
+++ nemo/collections/nlp/parts/mixins/nlp_adapter_mixins.py
@@ -203,8 +203,8 @@ class NLPAdapterModelMixin:
 
         if self.cfg.get('virtual_pipeline_model_parallel_size', None):
             raise ValueError('Virtual pipeline model parallel is not supported when using PEFT')
-        if self.cfg.optim.name == "distributed_fused_adam":
-            raise ValueError('distributed_fused_adam is not supported for PEFT. Please use fused_adam')
+        # if self.cfg.optim.name == "distributed_fused_adam":
+        #     raise ValueError('distributed_fused_adam is not supported for PEFT. Please use fused_adam')
 
         self.use_peft = True
         if not isinstance(peft_cfgs, List):
