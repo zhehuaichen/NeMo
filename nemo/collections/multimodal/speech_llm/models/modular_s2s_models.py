db1212c1dad3ba7c6086c0a26972c54f4a3b3f2d
zhehuaichen <dian.chenzhehuai@gmail.com>
2025-01-27 15:03:40 -0800

    Revert "add onelogger"
    This reverts commit c118621412831f12cd7d9102817ab16b82835bc8.
    
    Signed-off-by: zhehuaichen <dian.chenzhehuai@gmail.com>

--- nemo/collections/multimodal/speech_llm/models/modular_s2s_models.py
+++ nemo/collections/multimodal/speech_llm/models/modular_s2s_models.py
@@ -466,9 +466,6 @@ class S2sModularAudioGPTModel(ModularAudioGPTModel):
         return model
 
     def post_restore_from_pretrained_models(cls, model, cfg):
-
-        model = super().restore_from_pretrained_models(cfg, trainer)
-
         codec_model, codec_model_cfg = cls.get_codec_models_and_configs(cfg)
         logging.info(f"Loaded Codec Model: {codec_model}")
 
@@ -514,12 +511,8 @@ class S2sModularAudioGPTModel(ModularAudioGPTModel):
         cfg: Optional[Union[OmegaConf, str]] = None,
         trainer: Optional[Trainer] = None,
     ):
-        trainer.time_event_callback.logtimeevent.on_model_init_start()
         model = super().restore_from_pretrained_models(cfg, trainer)
-        trainer.time_event_callback.logtimeevent.on_model_init_end()
-        trainer.time_event_callback.logtimeevent.on_load_checkpoint_start()
         cls.post_restore_from_pretrained_models(cls, model, cfg)
-        trainer.time_event_callback.logtimeevent.on_load_checkpoint_end()
         return model
 
     def load_state_dict(self, state_dict, strict: bool = True):
