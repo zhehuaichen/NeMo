f31720176b25cb6d692d38948a76e1733ba0fa1b
zhehuaichen <dian.chenzhehuai@gmail.com>
2025-01-27 15:00:07 -0800

    Revert "small fix"
    This reverts commit d5718ef39f8cec3203637e8f0c00f3f3db34e973.

--- nemo/collections/multimodal/speech_llm/models/modular_s2s_models.py
+++ nemo/collections/multimodal/speech_llm/models/modular_s2s_models.py
@@ -467,6 +467,8 @@ class S2sModularAudioGPTModel(ModularAudioGPTModel):
 
     def post_restore_from_pretrained_models(cls, model, cfg):
 
+        model = super().restore_from_pretrained_models(cfg, trainer)
+
         codec_model, codec_model_cfg = cls.get_codec_models_and_configs(cfg)
         logging.info(f"Loaded Codec Model: {codec_model}")
 
